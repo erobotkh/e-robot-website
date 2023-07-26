925c7bde93473bea3e9d2abd5ecf7860620e878f
kimlang17 <kimlangso17@gmail.com>
2023-07-26 20:22:39 +0700

    modified

--- resources/views/detail_content.blade.php
+++ resources/views/detail_content.blade.php
@@ -8,7 +8,7 @@
 </head>
 <style type="text/tailwindcss">
     @layer utilities {
-        body{font-family: 'kantumruy pro','typography';}
+        body{font-family: 'Open Sans','typography';}
         }
         .like-button.active svg path {   
         fill: #B30F10;
@@ -17,28 +17,89 @@
 
 <body>
     <div class="md:flex md:p-5 ">
-        <div class="md:w-1/2 flex">
-            <div id="arrow-left" class="arrow cursor-pointer"><img
-                    src="https://www.svgrepo.com/show/384730/rounded-square-arrow-indicator-left.svg" alt=""
-                    class="w-[50px] mt-[100px]">
+        <div>
+            <div class="flex">
+                <div id="arrow-left" class="arrow cursor-pointer"><img
+                        src="https://www.svgrepo.com/show/384730/rounded-square-arrow-indicator-left.svg" alt=""
+                        class="w-[50px] mt-[100px]">
+                </div>
+                <div class="w-full">
+                    <div class="slide slide1"><img
+                            src="https://vendelux.com/wp-content/uploads/technology-events-2023-guide.png" alt="">
+                    </div>
+                    <div class="slide slide2"><img
+                            src="https://s31606.pcdn.co/wp-content/uploads/2021/04/iStock-1205300906-scaled.jpg" alt="">
+                    </div>
+                    <div class="slide slide3"><img
+                            src="https://unctad.org/sites/default/files/2021-02/tir2021_launchevent_1200x675.jpg"
+                            alt="">
+                    </div>
+                </div>
+                <div id="arrow-right" class="arrow cursor-pointer"><img
+                        src="https://www.svgrepo.com/show/384728/rounded-square-arrow-indicator-right.svg" alt=""
+                        class="w-[50px] mt-[100px]"></div>
             </div>
-            <div class="w-full">
-                <div class="slide slide1"><img
-                        src="https://vendelux.com/wp-content/uploads/technology-events-2023-guide.png" alt="">
+
+            <a href="#" class="like-button">
+                <button type=""
+                    class="my-5 ml-5 px-2 py-1 border-2 border-solid border-[#EB0100] text-white bg-[#EB0100] flex rounded-lg  hover:bg-[#ff7374]">
+                    <svg width="20" height="20" viewBox="0 0 1792 1792" class="fill-[#fff] mr-3"
+                        xmlns=" http://www.w3.org/2000/svg">
+                        <path
+                            d="M320 1344q0-26-19-45t-45-19q-27 0-45.5 19t-18.5 45q0 27 18.5 45.5t45.5 18.5q26 0 45-18.5t19-45.5zm160-512v640q0 26-19 45t-45 19h-288q-26 0-45-19t-19-45v-640q0-26 19-45t45-19h288q26 0 45 19t19 45zm1184 0q0 86-55 149 15 44 15 76 3 76-43 137 17 56 0 117-15 57-54 94 9 112-49 181-64 76-197 78h-129q-66 0-144-15.5t-121.5-29-120.5-39.5q-123-43-158-44-26-1-45-19.5t-19-44.5v-641q0-25 18-43.5t43-20.5q24-2 76-59t101-121q68-87 101-120 18-18 31-48t17.5-48.5 13.5-60.5q7-39 12.5-61t19.5-52 34-50q19-19 45-19 46 0 82.5 10.5t60 26 40 40.5 24 45 12 50 5 45 .5 39q0 38-9.5 76t-19 60-27.5 56q-3 6-10 18t-11 22-8 24h277q78 0 135 57t57 135z" />
+                    </svg>
+                    Like
+                </button>
+            </a>
+            <div class="flex justify-between">
+                <div class="flex">
+                    <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
+                        alt="" class="w-[50px] h-[50px] rounded-full">
+                    <div>
+                        <h1 class="font-bold pt-3 pl-3">Kimlang</h1>
+                        <h1 class="font-light text-xs pl-3">1day ago</h1>
+                    </div>
                 </div>
-                <div class="slide slide2"><img
-                        src="https://s31606.pcdn.co/wp-content/uploads/2021/04/iStock-1205300906-scaled.jpg" alt="">
-                </div>
-                <div class="slide slide3"><img
-                        src="https://unctad.org/sites/default/files/2021-02/tir2021_launchevent_1200x675.jpg" alt="">
+                <div>
+                    <div class="">
+                        <button class=" rounded inline-flex " type="button" data-dropdown-toggle="dropdown">
+
+                            <svg class=" w-8 " fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z">
+                                </path>
+                            </svg>
+                        </button>
+                        <div class=" hidden bg-white text-base list-none divide-y divide-gray-100 rounded shadow my-4 items-end px-5 -mt-8"
+                            id="dropdown">
+
+                            <ul class=" py-1" aria-labelledby="dropdown">
+                                <li><a class=" hover:text-blue-600" href="">Edit</a>
+                                </li>
+                                <li class="">
+
+                                    <div class=" hover:text-red-600">
+                                        <button class="btn btn-danger delete">Delete</button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div id="arrow-right" class="arrow cursor-pointer"><img
-                    src="https://www.svgrepo.com/show/384728/rounded-square-arrow-indicator-right.svg" alt=""
-                    class="w-[50px] mt-[100px]"></div>
+            <div class="text-justify">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit adipisci placeat assumenda ea facere
+                earum
+                cum, dolore recusandae corporis laborum cupiditate, aspernatur ratione dolorum optio, maxime
+                possimus
+                tempora necessitatibus repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                illo voluptatum deserunt sed expedita amet accusamus commodi magnam ullam debitis odit atque quia,
+                consequuntur delectus! Placeat magnam voluptatem provident ab.
+            </div>
         </div>
         <div class="md:w-1/2 p-5">
-            <div class="flex">
+            <!-- <div class="flex">
                 <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
                     alt="" class="w-[50px] h-[50px] rounded-full">
                 <div>
@@ -52,31 +113,44 @@
                 tempora necessitatibus repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
                 illo voluptatum deserunt sed expedita amet accusamus commodi magnam ullam debitis odit atque quia,
                 consequuntur delectus! Placeat magnam voluptatem provident ab.
-            </div>
+            </div> -->
             <div>
-                <a href="#" class="like-button">
-                    <button type=""
-                        class="px-2 py-1 border-2 border-solid border-[#504441] flex rounded-lg hover:bg-[#D4C3BF]">
-                        <svg width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M320 1344q0-26-19-45t-45-19q-27 0-45.5 19t-18.5 45q0 27 18.5 45.5t45.5 18.5q26 0 45-18.5t19-45.5zm160-512v640q0 26-19 45t-45 19h-288q-26 0-45-19t-19-45v-640q0-26 19-45t45-19h288q26 0 45 19t19 45zm1184 0q0 86-55 149 15 44 15 76 3 76-43 137 17 56 0 117-15 57-54 94 9 112-49 181-64 76-197 78h-129q-66 0-144-15.5t-121.5-29-120.5-39.5q-123-43-158-44-26-1-45-19.5t-19-44.5v-641q0-25 18-43.5t43-20.5q24-2 76-59t101-121q68-87 101-120 18-18 31-48t17.5-48.5 13.5-60.5q7-39 12.5-61t19.5-52 34-50q19-19 45-19 46 0 82.5 10.5t60 26 40 40.5 24 45 12 50 5 45 .5 39q0 38-9.5 76t-19 60-27.5 56q-3 6-10 18t-11 22-8 24h277q78 0 135 57t57 135z" />
-                        </svg>
-                        Like
-                    </button>
-                </a>
                 <div class="flex my-3">
                     <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
                         alt="" class="w-[50px] h-[50px] rounded-full">
                     <div class="flex">
                         <input type="text" placeholder="Write a Comment..."
-                            class="px-2 py-2 mt-1 ml-3 rounded-md bg-[#F9EEEC] w-[400px]">
-                        <button class="px-7 py-2 mt-1 ml-3 rounded-md bg-[#B30F10] font-bold text-white">Post</button>
+                            class="px-2 py-2 mt-1 ml-3 rounded-md bg-[#EBE0DD] w-[400px]">
+                        <button class="px-7 py-2 mt-1 ml-3 rounded-md bg-[#EB0100] font-bold text-white">Post</button>
                     </div>
                 </div>
                 <div class="font-bold text-lg">
                     Comments
                 </div>
-                <div class="flex bg-[#F9EEEC] p-2 rounded-md mb-1">
+                <div class="flex bg-[#EBE0DD] p-2 rounded-md mb-1">
+                    <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
+                        alt="" class="w-[50px] h-[50px] rounded-full">
+                    <div class="">
+                        <div class="flex">
+                            <h1 class="font-bold pl-3">Kimlang</h1>
+                            <h1 class="font-light pl-3">. 1day ago</h1>
+                        </div>
+                        <p class="pl-3">I think I like this so much</p>
+                        <a href="" id="reply-btn" class="text-xs ml-3">Reply</a>
+                        <a href="" class="text-xs text-red-500 ml-3">Delete</a>
+                        <div class="flex my-3 hidden" id="reply">
+                            <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
+                                alt="" class="w-[50px] h-[50px] rounded-full">
+                            <div class="flex">
+                                <input type="text" placeholder="Write a Comment..."
+                                    class="px-2 py-2 mt-1 ml-3 rounded-md bg-[#fff] w-[250px]">
+                                <button
+                                    class="px-7 py-2 mt-1 ml-3 rounded-md bg-[#EB0100] font-bold text-white">Post</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex bg-[#EBE0DD] p-2 rounded-md mb-1">
                     <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
                         alt="" class="w-[50px] h-[50px] rounded-full">
                     <div class="">
@@ -87,18 +161,7 @@
                         <p class="pl-3">I think I like this so much</p>
                     </div>
                 </div>
-                <div class="flex bg-[#F9EEEC] p-2 rounded-md mb-1">
-                    <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
-                        alt="" class="w-[50px] h-[50px] rounded-full">
-                    <div class="">
-                        <div class="flex">
-                            <h1 class="font-bold pl-3">Kimlang</h1>
-                            <h1 class="font-light pl-3">. 1day ago</h1>
-                        </div>
-                        <p class="pl-3">I think I like this so much</p>
-                    </div>
-                </div>
-                <div class="flex bg-[#F9EEEC] p-2 rounded-md mb-1">
+                <div class="flex bg-[#EBE0DD] p-2 rounded-md mb-1">
                     <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
                         alt="" class="w-[50px] h-[50px] rounded-full">
                     <div class="">
@@ -172,6 +235,13 @@ button.addEventListener("click", function(e) {
     this.classList.add("animated");
     generateClones(this);
 });
+
+let ReplyBtn = document.querySelector("#reply-btn");
+let Reply = document.querySelector("#reply");
+ReplyBtn.addEventListener("click", function() {
+    Reply.style.display = "flex";
+});
 </script>
+<script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
 
 </html>
--- resources/views/post_content.blade.php
+++ resources/views/post_content.blade.php
@@ -5,52 +5,81 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <style type="text/tailwindcss">
     @layer utilities {
       body {
-        font-family: "kantumruy pro", "typography";
+        font-family: "Open Sans", "typography";
       }
     }
   </style>
 
 <body class="grid">
-    <div class="grid place-self-center m-5 rounded-lg shadow-lg md:w-[800px] bg-[#F9EEEC] p-5">
-        <div class="flex pb-5 px-5">
-            <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
-                alt="" class="w-[50px] h-[50px] rounded-full">
-            <div>
-                <h1 class="font-bold pt-3 pl-3">Kimlang</h1>
+    <div class="grid place-self-center m-5 rounded-lg shadow-lg md:w-[800px] bg-[#E1E1E1] p-5">
+        <div class="flex pb-5 px-5 justify-between">
+            <div class="flex">
+                <img src="https://img.freepik.com/premium-photo/japanese-girl-cherry-blossom-tree-landscape-anime-manga-illustration_691560-7776.jpg"
+                    alt="" class="w-[50px] h-[50px] rounded-full">
+                <div>
+                    <h1 class="font-bold pt-3 pl-3 text-xl">Kimlang</h1>
+                </div>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                class="bi bi-backspace cursor-pointer" viewBox="0 0 16 16" id="IconChangeColor">
+                <path
+                    d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"
+                    id="mainIconPathAttribute"></path>
+                <path
+                    d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z"
+                    id="mainIconPathAttribute"></path>
+            </svg>
         </div>
         <form action="/uploadtodatabase" method="post" class="md:w-[700px] place-self-center">
             <div class=" ">
                 <div class="">
                     Pictures/Video
                 </div>
-                <div class=" lg:text-sm md:text-md text-md py-5">
-                    <input type="file" name="image" multiple="multiple" />
-                </div>
-            </div>
+                <div class="flex">
+                    <input type="file" id="file-input" name="image" multiple="multiple" class="hidden" />
+                    <label for="file-input"
+                        class="my-5 p-5 hover:bg-[#E1E1E1] hover:text-black transition-all duration-500 bg-[#978E8D] border-[#EB0100] text-white rounded-lg border-2 text-center cursor-pointer">
+                        <i class="fa-solid fa-file-arrow-up"></i>&nbsp; Choose Photos or Videos to upload
+                    </label>
+                    <ul id="files-list" class="py-10 mx-5">
 
-            <div class="">
+                    </ul>
+                </div>
+
                 <div class="">
-                    Write something:
+                    <div class="">
+                        Write something:
+                    </div>
+                    <div class="py-5">
+                        <input
+                            class="hover:border-red-600 focus:ring-red-600 focus:border-red-600 p-3 w-full h-full text-lg placeholder:text-lg border-2 rounded-lg"
+                            type="textarea" name="caption" placeholder="What's on your mind?" maxlength="1000" />
+                    </div>
+                    <div>
+                        <a href="#"><button
+                                class="px-7 py-2 mt-1 mr-5 rounded-md bg-[#000] font-bold text-white">Back</button></a>
+                        <button class="px-7 py-2 mt-1 mr-5 rounded-md bg-[#EB0100] font-bold text-white">Upload</button>
+                    </div>
                 </div>
-                <div class="py-5">
-                    <input
-                        class="hover:border-red-600 focus:ring-red-600 focus:border-red-600 p-3 w-full h-full text-lg placeholder:text-lg border-2 rounded-lg"
-                        type="textarea" name="caption" placeholder="What's on your mind?" maxlength="1000" />
-                </div>
-                <div>
-                    <button class="px-7 py-2 mt-1 rounded-md bg-[#635C5B] font-bold text-white">Back</button>
-                    <button class="px-7 py-2 mt-1 rounded-md bg-[#B30F10] font-bold text-white">Upload</button>
-                </div>
-            </div>
         </form>
     </div>
 
-    <script src="../js/script_post.js"></script>
+    <script>
+    let fileInput = document.getElementById("file-input");
+    let fileList = document.getElementById("files-list");
+    let numOfFiles = document.getElementById("num-of-files");
+
+    fileInput.addEventListener("change", () => {
+        fileList.innerHTML = `${fileInput.files.length} 
+        Files Selected`;
+        numOfFiles.innerHTML = ` `;
+    })
+    </script>
 </body>
 
 </html>
