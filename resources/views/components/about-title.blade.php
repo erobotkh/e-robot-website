52e27e91d282bae5c79b1986130b3c29fdb139e9
kimlong <kimlong5244@gmail.com>
2023-08-07 20:06:41 +0700

    all parts of about page

---
+++ resources/views/components/about-title.blade.php
@@ -0,0 +1,3 @@
+@props(['title'])
+
+<h2 class="mb-4 text-center text-4xl text-gray-900  md:text-6xl text-red-600 font-bold uppercase">{{$title}}</h2>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,9 +8,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{asset('styles/style.css')}}">
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
     <title>E-robot</title>
 </head>
 <body>
+    @include('./partials/_nav-bar')
+   <main>
     {{$slot}}
+   </main>
+   @include('./partials/_footer')
 </body>
 </html>
