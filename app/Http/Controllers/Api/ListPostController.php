592e91d11e2603373b1bf19287a84a9fd24beec8
Moon Li <kornsophea91@gmail.com>
2023-08-09 13:01:30 +0700

    post

--- app/Http/Controllers/Api/ListPostController.php
+++ app/Http/Controllers/Api/ListPostController.php
@@ -42,7 +42,6 @@ class ListPostController extends Controller
     {
         $datas = PostContent::orderBy('updated_at', 'desc')?->get();
         foreach ($datas as $data) {
-            $users = PostContent::find($data->user_id)?->user;
             $user_profiles = User::find($data->user_id)?->user_profiles;
             $likes = PostContent::find($data->id)?->likes;
             $comments = PostContent::find($data->id)?->comments;
@@ -52,7 +51,6 @@ class ListPostController extends Controller
                 $data['total_sub_comment'] += $comment['sub'];
             }
             $data['total_comment'] = $comments->count();
-            $data['user_name'] = $users->name;
             $data['user_profile'] = $user_profiles->profile_image_url;
             $data['like_number'] = $likes->count();
         }
