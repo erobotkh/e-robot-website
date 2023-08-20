15d2520f164176c2957a90f1232f1108d1756adf
kimlong <kimlong5244@gmail.com>
2023-08-20 16:17:09 +0700

    add values

--- resources/views/about/mission_vision.blade.php
+++ resources/views/about/mission_vision.blade.php
@@ -24,5 +24,31 @@
                 </p>
             </div>
         </div>
+        <div class="hidden md:inline-block h-100 w-2  bg-red-500"></div>
+        <div class="mt-4 p-2 w-full">
+            <h1 class="text-5xl font-bold text-center text-red-600">Values</h1>
+            <div class="w-[50%] sm:w-[40%] my-8 mx-auto cursor-pointer hover:w-[45%] transition-all duration-300 ease-in-out ">
+                <img class="object-fit" src={{asset('./images/mission-erobot.png')}} alt="vision-logo">
+            </div>
+            <div>
+                <p class="text-center">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse aspernatur sit dolorem quisquam optio. Beatae officiis nam nulla amet velit? Ab expedita reiciendis aut impedit quam quod, fuga deserunt praesentium.
+                  
+                </p>
+            </div>
+        </div>
     </div>
+    <!-- <div class="p-12 ">
+        <h2 class="text-center text-5xl font-bold text-center text-red-600 mb-5">Value</h2>
+        <p class="text-center mb-4">To achieve its vision and fulfil its mission, Erobot values the following:</p>
+        <div >
+            <table> 
+                <x-value-row title="Title" value="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi recusandae culpa, fugiat aliquam, quam sit omnis officiis necessitatibus commodi, velit placeat exercitationem molestiae! Fuga sed numquam, optio blanditiis sit delectus."/>
+                <x-value-row title="Title" value="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi recusandae culpa, fugiat aliquam, quam sit omnis officiis necessitatibus commodi, velit placeat exercitationem molestiae! Fuga sed numquam, optio blanditiis sit delectus."/>
+                <x-value-row title="Title" value="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi recusandae culpa, fugiat aliquam, quam sit omnis officiis necessitatibus commodi, velit placeat exercitationem molestiae! Fuga sed numquam, optio blanditiis sit delectus."/>
+                <x-value-row title="Title" value="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi recusandae culpa, fugiat aliquam, quam sit omnis officiis necessitatibus commodi, velit placeat exercitationem molestiae! Fuga sed numquam, optio blanditiis sit delectus."/>
+                <x-value-row title="Title" value="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi recusandae culpa, fugiat aliquam, quam sit omnis officiis necessitatibus commodi, velit placeat exercitationem molestiae! Fuga sed numquam, optio blanditiis sit delectus."/>
+            </table>
+        </div>
+    </div> -->
 </x-layout>
---
+++ resources/views/components/value-row.blade.php
@@ -0,0 +1,5 @@
+@props(['title', 'value'])
+<tr class="flex mb-4 gap-3">
+  <td> ✏️</td>
+   <td><b class="text-red-500">{{$title}}</b> - {{$value}}</td>
+</tr>
