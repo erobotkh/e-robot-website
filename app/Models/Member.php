ecc7e9f742a777ee0cabb46d0b3c8956e1a9c1e9
Moon Li <kornsophea91@gmail.com>
2023-09-09 12:35:53 +0700

    conflict

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -10,32 +10,21 @@ use Illuminate\Notifications\Notifiable;
 
 class Member extends Model
 {
-<<<<<<< HEAD
-    use HasFactory, Notifiable;
-    protected $fillable = [
-        'name',
-=======
     use HasFactory,Notifiable;
     protected $fillable=[
         'first_name',
         'last_name',
->>>>>>> 418f2a6a4101532b65b43a62ce71f9992e3c1d1a
         'bio',
         'profile',
         'member_position_id',
         'team_id'
     ];
-<<<<<<< HEAD
-    public function member_position(): HasOne
-    {
-=======
     protected $appends = ['profile_url'];
 
     public function getProfileUrlAttribute() {
         return env('DO_URL').$this->profile;
     }
     public function member_position():HasOne{
->>>>>>> 418f2a6a4101532b65b43a62ce71f9992e3c1d1a
         return $this->hasOne(MemberPosition::class);
     }
     public function socails(): HasMany
